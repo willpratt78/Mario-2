dc7da9766534f4369a7c668a51919e96eb798321
William Pratt <willpratt78@gmail.com>
2022-05-29 11:26:45 -0700

    validations for all instances made

--- app/models/review.rb
+++ app/models/review.rb
@@ -1,3 +1,8 @@
 class Review < ApplicationRecord
   belongs_to :product
+  validates :author, presence: true
+  validates :content_body, presence: true
+  validates :content_body, length: {in: 50..250}
+  validates :rating, presence: true
+  validates :content_body, numericality: {less_than: 6}
 end
