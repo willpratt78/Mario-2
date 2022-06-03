220f2f376341bb58e85d0629344b0ab45377b4c2
William Pratt <willpratt78@gmail.com>
2022-06-03 10:21:54 -0700

    validations for rating and content_boy are in place

--- app/models/review.rb
+++ app/models/review.rb
@@ -1,6 +1,6 @@
 class Review < ApplicationRecord
   belongs_to :product
   validates :author, presence: true
-  validates :content_body, presence: true
-  validates :rating, presence: true
+  validates :content_body, length: {in: 50..250}
+  validates :rating, numericality: {less_than: 6}
 end
