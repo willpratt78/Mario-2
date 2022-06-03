b02766d5932dea3f9cfba5f195dee45da1196b8f
William Pratt <willpratt78@gmail.com>
2022-06-03 15:09:22 -0700

    faker currently creates 50 fake products

--- Gemfile
+++ Gemfile
@@ -37,6 +37,8 @@ gem 'bootsnap', '>= 1.1.0', require: false
 
 gem 'jquery-rails'
 
+gem "faker"
+
 group :test do
   gem 'shoulda-matchers'
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,14 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+Product.destroy_all
+
+50.times do |index|
+  Product.create!(name: Faker::Food.spice,
+                        cost: Faker::Number.between(from: 0, to: 6),
+                        country_of_origin: Faker::Lorem.sentence(1, false, 0).chop)
+                        
+end
+
+p "Created #{Product.count} spices"
